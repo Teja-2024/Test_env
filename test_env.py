51077b6e8e181591672df55a28fb87c7643514bc
Teja-2024 <166777391+Teja-2024@users.noreply.github.com>
2024-09-16 16:22:02 +0530

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dotenv
 
 # Load .env file
 env_path = Path('.') / '.env'
@@ -8,10 +9,10 @@ load_dotenv(dotenv_path=env_path)
 
 # Accessing environment variables
 app_base_url = os.getenv('APP_BASE_URL')
-api_base_url = os.getenv('API_BASE_URL')
+api_base_url = os.environ('API_BASE_URL')
 api_users_url = os.getenv('API_USERS_URL')
 api_products_url = os.getenv('API_PRODUCTS_URL')
-api_orders_url = os.getenv('API_ORDERS_URL')
+api_orders_url = dotenv.get_key('API_ORDERS_URL')
 
 # Print the values
 print(f"App Base URL: {app_base_url}")
